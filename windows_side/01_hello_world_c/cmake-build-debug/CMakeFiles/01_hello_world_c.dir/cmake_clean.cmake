81ab34c22a20c006de20a8bcb01133794c4d8813
swcho84 <swcho84@gmail.com>
2021-09-01 20:49:39 +0900

    add env files

---
+++ windows_side/01_hello_world_c/cmake-build-debug/CMakeFiles/01_hello_world_c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "01_hello_world_c.exe"
+  "01_hello_world_c.exe.manifest"
+  "01_hello_world_c.lib"
+  "01_hello_world_c.pdb"
+  "CMakeFiles/01_hello_world_c.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01_hello_world_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
