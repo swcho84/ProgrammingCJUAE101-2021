42bb80de85b5cd094d8963d435986aec14ee219e
swcho84 <swcho84@gmail.com>
2021-09-07 10:40:06 +0900

    first upload, clion, in windows

---
+++ windows_side/00_helloworld_clion/cmake-build-debug/CMakeFiles/00_helloworld_clion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "00_helloworld_clion.exe"
+  "00_helloworld_clion.exe.manifest"
+  "00_helloworld_clion.lib"
+  "00_helloworld_clion.pdb"
+  "CMakeFiles/00_helloworld_clion.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/00_helloworld_clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
