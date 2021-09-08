f41196ec9f1122b1c17cc35e631758985561dd84
swcho84 <swcho84@gmail.com>
2021-09-08 17:20:36 +0900

    tested2

---
+++ windows_side/00_helloworld_clion_test/cmake-build-debug/CMakeFiles/00_helloworld_clion_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "00_helloworld_clion_test.exe"
+  "00_helloworld_clion_test.exe.manifest"
+  "00_helloworld_clion_test.lib"
+  "00_helloworld_clion_test.pdb"
+  "CMakeFiles/00_helloworld_clion_test.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/00_helloworld_clion_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
