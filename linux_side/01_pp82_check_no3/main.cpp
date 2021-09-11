70c6005bfce14ae283af602235335fab05c57852
swcho84 <swcho84@gmail.com>
2021-09-11 18:33:27 +0900

    work done: 2nd week problems

---
+++ linux_side/01_pp82_check_no3/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	// showing the inty, string, character-type value
+	printf("Student ID: %d\n", 20125289);
+	printf("Name: %s\n", "SungwookCho");
+	printf("Grade: %c\n", 'A');
+
+	// pause function (windows: system("pause");
+	getchar();
+  return 0;
+}
